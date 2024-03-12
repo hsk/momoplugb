84a8d308f46efb85d65f207bf6e5afd7d3791bbf
hsk <sakurai777@gmail.com>
2024-03-12 17:29:30 +0900

    3

---
+++ 3.pl
@@ -0,0 +1,6 @@
+eval(I,I)    :- integer(I).                          % 整数
+eval(E1+E2,I):- eval(E1,I1),eval(E2,I2),I is I1+I2.  % 加算
+eval(E1-E2,I):- eval(E1,I1),eval(E2,I2),I is I1-I2.  % 減算
+:- eval(1+2+3+4,R),writeln(R).
+:- eval((10+20)-(3+4)-2,R),writeln(R).
+:- halt.
